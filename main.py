b0d18e46f0a38a5d02369b7bb2051f28d4a11ff5
Vinícius Azevedo <vmeazevedo@gmail.com>
2021-01-14 22:28:19 -0300

    add funções de add e ler dados da base de dados

--- main.py
+++ main.py
@@ -28,3 +28,119 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS Crud")
 cursor.execute("use Crud")
 cursor.execute("CREATE TABLE IF NOT EXISTS Pessoas (ID int auto_increment, Nome varchar(50) not null, cpf varchar(50) not null, Data TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, primary key (id))")
 ###############################################################################################################
+
+###############################################################################################################
+# CREATE
+###############################################################################################################
+def create():
+    cursor = db_connection.cursor()
+    while True:
+        Nome = input("Digite o seu nome: ").title()
+        try:
+            Nome = str(Nome)
+            if Nome.isdigit():
+                print("Por favor insira um nome válido.")
+            elif Nome.isspace():
+                print("Por favor insira um nome válido.")
+            else :
+                break
+        except:
+            print("Por favor, utilize apenas letras.")
+    while True:
+        cpf = input("Digite o seu CPF: ")
+        try:
+            cpf = int(cpf)
+            if cpf < 0:
+                print("O número não pode ser negativo")
+            elif len(str(cpf)) < 11:
+                print("Você digitou números a menos em seu CPF")
+            else:
+                break
+        except:
+            print("Por favor insira um CPF válido.")
+
+    #Realizando a inserção dos dados em nosso banco de dados
+    sql = "INSERT INTO Pessoas (Nome, CPF) VALUES ('{}','{}')".format(Nome, cpf)
+    cursor.execute(sql)
+    db_connection.commit()
+    print("\nDados inseridos com sucesso")
+###############################################################################################################
+
+###############################################################################################################
+# READ
+###############################################################################################################
+def read():
+    cursor = db_connection.cursor()
+    while True:
+        print("\n")
+        print("="*20)
+        print("       MENU")
+        print("="*20)
+        print("Você deseja consultar: ")
+        print("1 - Nome")
+        print("2 - CPF")
+        print("3 - A base toda")
+        choose = input("\nDigite a opção desejada: ")
+        try:
+            choose = int(choose)
+            if choose < 0:
+                print("Por favor digite uma opção válida!")
+            elif choose > 3:
+                print("Por favor digite uma opção válida!")
+            elif choose == 0:
+                print("Por favor digite uma opção válida!")
+            elif choose == 1:
+                while True:
+                    Nome = input("\nDigite o nome a ser localizado: ").title()
+                    try:
+                        Nome = str(Nome)
+                        if Nome.isdigit():
+                            print("Por favor insira um nome válido.")
+                        elif Nome.isspace():
+                            print("Por favor insira um nome válido.")
+                        else :
+                            break
+                    except:
+                        print("Por favor, utilize apenas letras.")
+                        break
+                sql = "SELECT * FROM Pessoas WHERE Nome = '{}'".format(Nome)
+                cursor.execute(sql)
+                dados_lidos = cursor.fetchall()
+                if dados_lidos == []:
+                    print("Erro: Não encontramos {} em nossa base.". format(Nome))
+                else:
+                    print(dados_lidos)
+                    print("\nDados apresentados com sucesso.")
+                    break
+
+            elif choose == 2:
+                while True:
+                    cpf = input("\nDigite o CPF a ser localizado: ")
+                    try:
+                        cpf = int(cpf)
+                        if cpf < 0:
+                            print("O número não pode ser negativo")
+                        elif len(str(cpf)) < 11:
+                            print("Você digitou números a menos em seu CPF")
+                        else :
+                            break
+                    except:
+                        print("Por favor, utilize apenas números.")
+                        
+                sql = "SELECT * FROM Pessoas WHERE cpf = '{}'".format(cpf)
+                cursor.execute(sql)
+                dados_lidos = cursor.fetchall()
+                if dados_lidos == []:
+                    print("Erro: Não encontramos {} em nossa base.". format(cpf))
+                else:
+                    print(dados_lidos)
+                    print("\nDados apresentados com sucesso.")
+                    break
+        except:
+            print("Por favor utilize apenas números.")
+###############################################################################################################
+
+
+# create()
+read()
+
